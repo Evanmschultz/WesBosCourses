543bd72408c54e54681a3553df7a7de80deb3617
Evan <evanmschultz@gmail.com>
2023-06-24 17:17:03 -0600

    completes challenge

--- JavaScript30/02 - JS and CSS Clock/script.js
+++ JavaScript30/02 - JS and CSS Clock/script.js
@@ -1,23 +1,25 @@
-const secondsHand = document.getElementById("seconds-hand")
-const minuteHand = document.getElementById("minute-hand")
-const hourHand = document.getElementById("hour-hand")
+const secondsHand = document.getElementById('seconds-hand')
+const minuteHand = document.getElementById('minute-hand')
+const hourHand = document.getElementById('hour-hand')
 
 function updateHandAngles() {
-    const now = new Date()
+	const now = new Date()
 
-    const seconds = now.getSeconds()
-    const secondsHandAngle = (seconds % 60) * 6 + 90 // 360 / 60 = 6, 1 second = 6° // add 90° to compensate for initial transform
-    secondsHand.style.transform = `rotate(${secondsHandAngle}deg)`
+	const seconds = now.getSeconds()
+	const secondsHandAngle = (seconds % 60) * 6 + 90 // 360 / 60 = 6, 1 second = 6° // add 90° to compensate for initial transform
+	secondsHand.style.transform = `rotate(${secondsHandAngle}deg)`
 
-    const minute = now.getMinutes()
-    const minuteHandAngle = (minute % 60) * 6 + 90
-    minuteHand.style.transform = `rotate(${minuteHandAngle}deg)`
+	const minute = now.getMinutes()
+	const minuteHandAngle = (minute % 60) * 6 + 90
+	minuteHand.style.transform = `rotate(${minuteHandAngle}deg)`
 
-    const hours = now.getHours()
-    const hourHandAngle = (hours % 12) * 30 + 90 // 360 / 12 = 30, 1 hour = 30°
-    hourHand.style.transform = `rotate(${hourHandAngle}deg)`
+	const hours = now.getHours()
+	const hourHandAngle = (hours % 12) * 30 + 90 // 360 / 12 = 30, 1 hour = 30°
+	hourHand.style.transform = `rotate(${hourHandAngle}deg)`
 }
 
 setInterval(() => {
-    updateHandAngles()
+	updateHandAngles()
 }, 1000)
+
+// challenge complete
