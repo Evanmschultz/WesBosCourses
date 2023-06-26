c56b5ea4fe63b835d1ff91d5c63b81c73b221eba
Evan <evanmschultz@gmail.com>
2023-06-26 16:57:55 -0600

    adds comments to solidify learning

--- JavaScript30/06 - Type Ahead/script.js
+++ JavaScript30/06 - Type Ahead/script.js
@@ -3,11 +3,13 @@ const endpoint =
 
 const cities = []
 
+// make a fetch request from the endpoint
 const promise = fetch(endpoint)
+	// convert what is returned to JSON, blob is an arbitrary name for the unorganized object returned
 	.then((blob) => blob.json())
+	/* 	push data to the cities array. using the spread operator `...` pushes only the current index instead 
+		of the whole array each time */
 	.then((data) => cities.push(...data))
-
-// console.log(cities)
 
 function findMatches(wordToMatch, cities) {
 	return cities.filter((place) => {
@@ -23,10 +25,17 @@ function numberWithCommas(num) {
 
 function displayMatches() {
 	const matchArray = findMatches(this.value, cities)
+
 	const html = matchArray
 		.map((place) => {
+			/*
+				regex means 'regular expression in JS and is used for searching for a 'pattern' match
+				the `new` statement is just creating a new instance of a regex. it creates a new object and 
+				calls the constructor function `RegExp` to initialize it.
+			*/
 			const regex = new RegExp(this.value, 'gi')
 
+			// cityName and stateName add the highlighting CSS class to the keyword input into the search bar
 			const cityName = place.city.replace(
 				regex,
 				`<span class="hl">${this.value}</span>`
@@ -36,6 +45,7 @@ function displayMatches() {
 				`<span class="hl">${this.value}</span>`
 			)
 
+			// returns html with new styling to be displayed in the results
 			return `
 				<li>
 					<span class="name">${cityName}, ${stateName}</span>
@@ -44,11 +54,14 @@ function displayMatches() {
 					`
 		})
 		.join('')
+
 	suggestions.innerHTML = html
 }
 
+// suggestions can be down here because the functions using them aren't called until below
 const searchInput = document.querySelector('.search')
 const suggestions = document.querySelector('.suggestions')
 
+// event listeners to run filter when strings are input in the search field
 searchInput.addEventListener('change', displayMatches)
 searchInput.addEventListener('keyup', displayMatches)
